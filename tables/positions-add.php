1af0ff20f91ca6f9084fac26957d36c6d4c0b31c
ChadTubil <46648145+ChadTubil@users.noreply.github.com>
2021-02-04 21:41:16 +0800

    commit

--- tables/positions-add.php
+++ tables/positions-add.php
@@ -91,10 +91,10 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="upload/<?php print $fetchUsers["users_image"]?>" class="img-circle elevation-2" style="width: 40px; height: 40px;">
         </div>
         <div class="info">
-          <a href="../profile.php" class="d-block"><?php print ucwords($_SESSION["users_firstname"])." ".ucwords($_SESSION["users_middlename"])." ".ucwords($_SESSION["users_lastname"]); ?></a>
+          <a href="../profile.php" class="d-block"><?php print $fetchUsers["users_firstname"]?></a>
         </div>
       </div>
 
